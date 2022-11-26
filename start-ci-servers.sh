cf1ea0008914b6264479e4549b3df75407c464f9
jxtSamFrimpong <hub4realtil@gmail.com>
2022-11-26 05:31:55 +0000

    still gitlab issues

--- start-ci-servers.sh
+++ start-ci-servers.sh
@@ -3,4 +3,7 @@
 chmod +x build-jenkins-dock.sh
 ./build-jenkins-dock.sh
 
-sudo docker compose -f docker-compose.yml up -d --remove-orphans --build
+chmod +x make-.gitlab.sh
+./make-.gitlab.sh
+
+docker compose -f docker-compose.yml up -d --remove-orphans --build
